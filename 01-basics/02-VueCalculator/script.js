ab5bf1c62f9621b17c7503009c47f54ba9ba9395
Dasha <d.sumina@mail.ru>
2023-07-12 00:05:33 +0300

    task 2.2 VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const operations = [
+  {
+    name: 'sum',
+    value: ' ➕',
+    operation: (a, b) => +a + +b
+  },
+  {
+    name: 'subtract',
+    value: ' ➖',
+    operation: (a, b) => a - b
+  },
+  {
+    name: 'multiply',
+    value: ' ✖',
+    operation: (a, b) => a * b
+  },
+  {
+    name: 'divide',
+    value: ' ➗',
+    operation: (a, b) => a / b
+  }
+]
+
+const vm = createApp({
+  data() {
+    return {
+      result: 0
+    };
+  },
+  created() {
+    this.operations = operations
+  },
+  methods: {
+    calculate (operation) {
+      const result = operation(this.$refs.firstNumber.value, this.$refs.secondNumber.value)
+
+      return this.result = isFinite(result) ? result : 0
+    }
+  }
+}).mount('#app');
