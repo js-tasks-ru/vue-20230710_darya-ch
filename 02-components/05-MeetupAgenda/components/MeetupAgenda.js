4b49a0bb77e9f7ab54d2ab475b6b10a138f45fe2
Dasha <d.sumina@mail.ru>
2023-07-19 00:05:39 +0300

    task 3.5 Component MeetupAgenda

--- 02-components/05-MeetupAgenda/components/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgenda.js
@@ -1,13 +1,25 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  components: { MeetupAgendaItem },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+      default: () => []
+    }
+  },
+
   template: `
     <ul class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <template v-for="(agendaItem, index) in agenda" :key="\`MeetupAgenda-\${index}\`">
+          <MeetupAgendaItem :agenda-item="agendaItem" />
+        </template>
       </li>
     </ul>`,
 });
