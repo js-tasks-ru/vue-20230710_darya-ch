1811fe6b7fc196890ac7c03f4bff08bbed3e770b
Dasha <d.sumina@mail.ru>
2023-07-19 03:55:26 +0300

    task 3.7 Component PageMeetup.

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,7 +1,8 @@
-import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { defineComponent, callWithAsyncErrorHandling } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import MeetupView from '../../06-MeetupView/components/MeetupView.js';
+import { fetchMeetupById } from '../meetupService.js';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,65 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView
   },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+      default: 1
+    }
+  },
+
+  data() {
+    return {
+      meetup: null,
+      error: 'error',
+      isLoading: true
+    };
+  },
+
+  watch: {
+    meetupId () {
+      this.getMeetup();
+    }
+  },
+
+  mounted () {
+    this.getMeetup();
+  },
+
+  methods: {
+    getMeetup () {
+      this.isLoading = true;
+
+      fetchMeetupById(this.meetupId)
+        .finally(() => this.isLoading = false)
+        .then(data => this.meetup = data)
+        .catch((error) => {
+          this.error = error.message;
+          this.meetup = null;
+          this.isLoading = false;
+        })
+    }
+  },
+
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
 
-      <UiContainer>
+      <MeetupView
+        v-if="meetup && !isLoading"
+        :meetup="meetup"
+      />
+
+      <UiContainer v-else-if="isLoading">
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-else >
+        <UiAlert>{{ error }}</UiAlert>
       </UiContainer>
     </div>`,
 });
